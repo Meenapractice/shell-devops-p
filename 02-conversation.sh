e496ad87cc935f5031318798e5c2c410d086a3de
meena_test <abc@gmail.com>
2026-04-18 05:25:23 +0530

    added code

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,4 +3,4 @@
 echo "SURESH: Hi RAMESH, How are you?"
 echo "RAMESH: Hi SURESH, I am fine"
 echo "SURESH: what are you studying RAMESH"
-echo "RAMESH" I am studying shell scripting SURESH"
+echo "RAMESH: I am studying shell scripting SURESH"
