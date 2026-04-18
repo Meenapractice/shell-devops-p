3df2009351ef26b624b69190289bd04c68d0d54d
meena_test <abc@gmail.com>
2026-04-18 05:45:12 +0530

    added code

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+NUM1=$1
+NUM2=$2
+
+if [ $# -ne 2 ]; then
+  echo "2 arguments are required"
+fi
+
+if [ $NUM1 -lt $NUM2 ]; then
+    echo "$NUM2 is greater than $NUM1"
+elif [ $NUM1 -eq $NUM2 ]; then
+  echo" $NUM1 and $NUM2 are equal"
+else
+  echo "$NUM1 is greater than $NUM2"
+fi
