0e1a3f0d318dafe9c4931b0fe16bebd41838f6bd
meena_test <abc@gmail.com>
2026-04-18 05:51:14 +0530

    added code

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 NUM1=$1
 NUM2=$2
 
---
+++ 10-installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(user -u)
+
+if [ $? -ne 0 ]; then
+  echo "Please run the script with root user"
+  exit 1
+fi
+
+dnf install nginx
+if[ $? -ne 0 ]; then
+  echo "Nginx not installed..Installing..SUCESS"
+else
+  echo "Nginx already installed.. FAILURE"
+fi
+
+dnf install mysql
+if[ $? -ne 0 ]; then
+  echo "mysql not installed..Installing..SUCESS"
+else
+  echo "mysql already installed.. FAILURE"
+fi
+
+dnf install nodejs
+if[ $? -ne 0 ]; then
+  echo "nodejs not installed..Installing..SUCESS"
+else
+  echo "nodejs already installed.. FAILURE"
+fi
+
