f6d838c7efc574f52590d48329667b123c3ae971
meena_test <abc@gmail.com>
2026-04-18 05:41:18 +0530

    added code

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -8,10 +8,10 @@ SUM=$(($NUM1 + $NUM2))
 echo "Sum is $SUM"
 
 FRUITS=("Apple"  "Banana" "Mango")
-echo "All fruits: $FRUITS[@]"
-echo "$FRUITS[0]"
-echo "$FRUITS[1]"
-echo "$FRUITS[2]"
-echo "$FRUITS[3]"
+echo "All fruits: ${FRUITS[@]}"
+echo "${FRUITS[0]}"
+echo "${FRUITS[1]}"
+echo "${FRUITS[2]}"
+echo "${FRUITS[3]}"
 
 
