9fcb2972345b1cd3d773435189a3f99510994d02
meena_test <abc@gmail.com>
2026-04-18 05:46:39 +0530

    added code

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -9,7 +9,7 @@ fi
 if [ $NUM1 -lt $NUM2 ]; then
     echo "$NUM2 is greater than $NUM1"
 elif [ $NUM1 -eq $NUM2 ]; then
-  echo" $NUM1 and $NUM2 are equal"
+  echo "$NUM1 and $NUM2 are equal"
 else
   echo "$NUM1 is greater than $NUM2"
 fi
