d2347ea9d8d0513f52aeb7a16a9fa73152c71005
meena_test <abc@gmail.com>
2026-04-20 06:49:17 +0530

    added code

--- 17-colors.sh
+++ 17-colors.sh
@@ -12,28 +12,28 @@ N='\e[0m'
 mkdir -p $LOGS_FOLDER
 
 if [ $USERID -ne 0 ]; then
-  echo "$R Please run the script with root access $N" | tee -a $LOGS_FILE
+  echo -e "$R Please run the script with root access $N" | tee -a $LOGS_FILE
   exit 1
 fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo "Installing $2.... $R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "Installing $2.... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "Installing $2.... $G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "Installing $2.... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
 for package in $@; do
   dnf list installed $package &>>LOGS_FILE
   if [ $? -ne 0 ]; then
-    echo "$package not installed..$G Installing now $N" | tee -a $LOGS_FILE
+    echo -e "$package not installed..$G Installing now $N" | tee -a $LOGS_FILE
     dnf install $package -y &>>LOGS_FILE
     VALIDATE $? $package
     exit 1
   else
-    echo "$package already installed..$Y SKIPPING $N" | tee -a $LOGS_FILE
+    echo -e "$package already installed..$Y SKIPPING $N" | tee -a $LOGS_FILE
   fi
   done
     
