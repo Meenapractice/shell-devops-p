a849285f599dd0d4e8a495f4352c5406631f33cd
meena_test <abc@gmail.com>
2026-04-20 06:39:30 +0530

    added code

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-USERID="$(id -u)"
+USERID=$(id -u)
 LOGS_FOLDER="/home/ec2-user/repos/shell-devops-p"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 mkdir -p $LOGS_FOLDER
 
-if [ USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
   echo "Please run the script with root access" | tee -a $LOGS_FILE
   exit 1
 fi
